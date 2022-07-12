e1b2b2de89d6a8f219dc2b87a5e6cc6ff1ce82d4
Bekir - CDI <b.burakcebecii@gmail.com>
2022-07-12 20:53:49 +0200

    frontend is ready to use. but not working because of ackend url..

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,26 @@ import { useState, useEffect } from "react";
 import "./App.scss";
 import axios from "axios";
 
+const backend_url = import.meta.env.VITE_BACKEND_URL;
+
 function App() {
+  const [jobSources, setJobSources] = useState([]);
+
+  useEffect(() => {
+    (async () => {
+      setJobSources((await axios.get(backend_url)).data);
+    })();
+  }, []);
+
   return (
     <div className="App">
-      <h1>BBC Job Manager</h1>
-      <p>Welcome to this site.</p>
+      <h1>EJ2 Job Manager</h1>
+      <p>There are {jobSources.length} job sources:</p>
+      <ul>
+        {jobSources.map((jobSource, i) => {
+          return <li key={i}>{jobSource.name}</li>;
+        })}
+      </ul>
     </div>
   );
 }
